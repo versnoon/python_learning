22f695192842eb5f6c6f2df14c620c3508a86812
versnoon <versnoon@gmail.com>
2021-04-14 17:22:05 +0800

    queue simulation print

--- src/commons/queue.py
+++ src/commons/queue.py
@@ -9,6 +9,9 @@
 '''
 
 
+import random
+
+
 def hot_potato(name_list, count):
     q = Queue()
     for name in name_list:
@@ -21,6 +24,33 @@ def hot_potato(name_list, count):
         q.dequeue()
 
     return q.dequeue()
+
+
+def create_new_task():
+    num = random.randrange(1, 181)
+    if num == 180:
+        return True
+    return False
+
+
+def simulation(num_seconds, pages_per_minute):
+    labprinter = Printer(pages_per_minute)
+    print_queue = Queue()
+    wait_times = []
+
+    for current_second in range(num_seconds):
+        if create_new_task():
+            task = Task(current_second)
+            print_queue.enqueue(task)
+        if (not labprinter.busy() and (not print_queue.is_empty())):
+            next_task = print_queue.dequeue()
+            wait_times.append(next_task.wait_times(current_second))
+            labprinter.start_next(next_task)
+        labprinter.tick()
+
+    average_wait = sum(wait_times) / len(wait_times)
+    print("Average Wait %6.2f secs %3d tasks remaining." %
+          (average_wait, print_queue.size()))
 
 
 class Queue:
@@ -39,3 +69,46 @@ class Queue:
 
     def size(self):
         return len(self.items)
+
+
+class Printer:
+    def __init__(self, ppm):
+        self.pagerate = ppm
+        self.current_task = None
+        self.time_remaining = 0
+
+    def tick(self):
+        if self.current_task != None:
+            self.time_remaining = self.time_remaining - 1
+            if self.time_remaining <= 0:
+                self.current_task = None
+
+    def busy(self):
+        if self.current_task != None:
+            return True
+        return False
+
+    def start_next(self, new_task):
+        self.current_task = new_task
+        self.time_remaining = new_task.get_pages() * 60 / self.pagerate
+
+
+class Task:
+
+    def __init__(self, time):
+        self.timestamp = time
+        self.pages = random.randrange(1, 21)
+
+    def get_stamp(self):
+        return self.timestamp
+
+    def get_pages(self):
+        return self.pages
+
+    def wait_times(self, current_time):
+        return current_time - self.timestamp
+
+
+if __name__ == "__main__":
+    for i in range(10):
+        simulation(3600, 10)
