2f14c6cadf237be842f0cd61bd2df26654addea5
versnoon <versnoon@gmail.com>
2021-10-26 10:20:06 +0800

    增加机构信息读取相关功能及测试

---
+++ src/salarys/depart.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   depart.py
+@Time    :   2021/10/26 09:00:29
+@Author  :   Tong tan 
+@Version :   1.0
+@Contact :   tongtan@gmail.com
+'''
+
+
+import pandas as pd
+import src.salarys.utils as utils
+import src.pandas.read_xls as prx
+
+
+class Depart:
+
+    def __init__(self) -> None:
+        self.name = ''  # 机构名称
+        self.display_name = ''  # 机构显示名称
+        self.children_names = []  # 相关单位
+        self.scope_no = ''  # 薪酬范围
+        self.sort_no = 99  # 显示顺序
+        self.tax_dep_name = ''  # 税务机构
+        self.gjj_dep_name = ''  # 公积金类别
+        self.sep = '|'  # 默认机构间分隔符
+
+
+class Departs:
+
+    def __init__(self, period=None) -> None:
+        self.departs = []
+        self.period = period
+        self.df = None
+        self.name = '审核机构信息'
+
+    def get_departs(self):
+        if not self.period:
+            raise ValueError(f'请指定期间信息')
+        if not self.df:
+            self.df = prx.make_df_from_excel_files(
+                period=self.period, file_root_path=utils.root_dir, file_name_prefix=self.name)
+        self.departs = self.to_depart_infos(self.df)
+
+    def to_depart_infos(self, df: pd.DataFrame) -> list:
+        return list(map(lambda s: self.to_depart_info(s), df.values.tolist()))
+
+    def to_depart_info(self, s) -> Depart:
+        d = Depart()
+        d.sort_no = s[0]
+        d.scope_no = s[1]  # 工资范围
+        d.display_name = s[2]  # 机构显示名称
+        d.name = s[3]  # ehr系统中单位名称
+        if s[4]:
+            if isinstance(s[4], str):
+                d.children_names = s[4].split(d.sep)  # 相关单位
+        d.tax_dep_name = s[6]  # 税务机构
+        d.gjj_dep_name = s[7]  # 公积金类型
+        return d
--- src/tests/test_salarys.py
+++ src/tests/test_salarys.py
@@ -9,16 +9,27 @@
 '''
 
 
-import src.salarys.period as period
+import src.salarys.period as period_op
+import src.salarys.depart as depart_op
 
 
 class TestSalarys:
 
     def test_period(self):
-        p = period.Period()
+        p = period_op.Period()
         p.get_period()
         assert p.df is not None
         assert p.year != 9999
         p.change_period(2021, 11)
         assert p.year == 2021
         assert p.month == 11
+
+    def test_departs(self):
+        ds = depart_op.Departs(period='202111')
+        ds.get_departs()
+        assert ds.df is not None
+        assert len(ds.departs) > 0
+        assert ds.departs[0].name == '马钢（集团）控股有限公司(总部)'
+        assert len(ds.departs[0].children_names) > 0
+        assert '办公室（党委办公室）' in ds.departs[0].children_names
+        assert ds.departs[0].display_name == '集团机关'
